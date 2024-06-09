fb10d6db65c5484e22632dde6cd76262c9de6d72
limpest0 <stealtridayth@gmail.com>
2024-06-09 11:52:22 +0300

    Add files via upload

--- gameshyter/gameshyter.py
+++ gameshyter/gameshyter.py
@@ -1,39 +1,37 @@
-import pygame.time
 import pygame
 import sys
 import math
 
-# Инициализация Pygame
 pygame.init()
 
-# Задаем размеры окна
 width, height = 900, 500
 screen = pygame.display.set_mode((width, height))
-
 pygame.display.set_caption("shyter")
 
-# Загрузка изображения фона
 background_image = pygame.image.load('Background.png')
 
-# Загрузка изображения, которое будет поверх фона
 foreground_image = pygame.image.load('boww.png')
+new_boww_width = 75
+new_boww_height = 75
+foreground_image = pygame.transform.scale(foreground_image, (new_boww_width, new_boww_height))
 
-arrow_image = pygame.image.load('arrow.png')
+arrow_image = pygame.image.load('yyy.png')
 
-# Установка границ угла (в градусах)
+clock = pygame.time.Clock()
+
 min_angle = -45
 max_angle = 90
 
-
 class Arrow:
     def __init__(self, x, y, angle):
-        self.image = pygame.transform.scale(arrow_image, (50, 10))
-        self.angle = math.radians(angle)  # Угол в радианах
+        self.image = pygame.transform.scale(arrow_image, (50, 50))
         self.rect = self.image.get_rect(center=(x, y))
-        self.speed = 3 # Скорость стрелы
+        self.speed = 15  # Скорость стрелы
+        self.angle = math.radians(angle)  # Угол в радианах
+        # Расчет компонент скорости по осям x и y с учетом угла
         self.x_velocity = self.speed * math.cos(self.angle)
-        # Учитываем, что ось y направлена вниз
         self.y_velocity = -self.speed * math.sin(self.angle)
+
 
     def update(self):
         self.rect.x += self.x_velocity
@@ -42,85 +40,64 @@ class Arrow:
     def draw(self, screen):
         screen.blit(self.image, self.rect.topleft)
 
+class Enemy:
+    def __init__(self, x, y, image_path):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load(image_path)
+        self.rect = self.image.get_rect()
 
-# Список стрел
+    def move(self):
+        self.x -= 1  
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
+
 arrows = []
+enemies = []  
+enemy_image_path = "enemy.png"
+enemy = Enemy(800, 200, enemy_image_path)
+enemies.append(enemy)  
 
-# Координаты для изображения, которое будет поверх фона
-foreground_x, foreground_y = 155, 330
-
-# Новые размеры для изображения
-new_width, new_height = 85, 85  # Укажите нужные размеры
-
-# Масштабирование изображения до новых размеров
-foreground_image = pygame.transform.scale(
-    foreground_image, (new_width, new_height))
-image_rect = foreground_image.get_rect(center=(foreground_x, foreground_y))
-
-
-# Задержка между выстрелами (в миллисекундах)
-shoot_delay = 500  # Например, задержка в полсекунды
-
-# Переменная для отслеживания времени последнего выстрела
-last_shot_time = 0
-
-# Основной цикл игры
-# Основной цикл игры
 running = True
+last_shot_time = 0
+shoot_delay = 500
+initial_character_x = 155
+initial_character_y = 330
+image_rect = foreground_image.get_rect(center=(initial_character_x, initial_character_y))
+
 while running:
-    # Получаем текущее время в миллисекундах
     current_time = pygame.time.get_ticks()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False  # Завершаем игру
+            running = False
 
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            # Проверяем прошла ли достаточная задержка с момента предыдущего выстрела
             if current_time - last_shot_time > shoot_delay:
-                # Получение позиции мыши
                 mouse_x, mouse_y = pygame.mouse.get_pos()
-
-                # Вычисление угла между центром изображения и позицией мыши
                 rel_x, rel_y = mouse_x - image_rect.centerx, mouse_y - image_rect.centery
-                # Угол между центром и курсором мыши
                 angle = math.degrees(math.atan2(-rel_y, rel_x))
 
-                # Создание новой стрелы с вычисленным углом направления
-                new_arrow = Arrow(image_rect.centerx,
-                                  image_rect.centery, angle)
+                new_arrow = Arrow(image_rect.centerx, image_rect.centery, angle)
                 arrows.append(new_arrow)
-
-                # Обновляем время последнего выстрела
                 last_shot_time = current_time
 
-    # Отрисовка изображения фона
     screen.blit(background_image, (0, 0))
 
-    # Получение позиции мыши
     mouse_x, mouse_y = pygame.mouse.get_pos()
-
-    # Вычисление угла между центром изображения и позицией мыши
     rel_x, rel_y = mouse_x - image_rect.centerx, mouse_y - image_rect.centery
-    angle = math.degrees(math.atan2(-rel_y, rel_x)) - \
-        -45  # Корректируем угол на 90 градусов
-
-    # Ограничиваем угол в пределах заданных границ
+    angle = math.degrees(math.atan2(-rel_y, rel_x)) - -45
     angle = max(min(angle, max_angle), min_angle)
-
-    # Поворот изображения
     rotated_image = pygame.transform.rotate(foreground_image, angle)
     rotated_rect = rotated_image.get_rect(center=image_rect.center)
-
-    # Отрисовка вращенного изображения поверх фона
     screen.blit(rotated_image, rotated_rect.topleft)
 
     for arrow in arrows:
         arrow.update()
         arrow.draw(screen)
 
-    # Обновляем экран
-    pygame.display.flip()
-
-# После выхода из цикла игры закрываем Pygame
-pygame.quit()
+    for enemy in enemies:
+        enemy.move()
+        enemy.draw(screen)
+    clock.tick(60)
+    pygame.display.update()
